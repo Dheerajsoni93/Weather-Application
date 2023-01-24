40b7da64ad76cf8032784c21406f21ce9606b0ce
Dheeraj Soni <dheerajsoni7023@gmail.com>
2023-01-24 12:25:21 +0530

    add the current-weather component

--- src/api.js
+++ src/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "a7282a0460fa5857fb76fb9da4f7344f";
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,18 +1,27 @@
-import { useState } from "react";
-import { GEO_API_URL, geoApiOptions } from '../../api'
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from '../../api';
 
 
-const Search = ({onSearchChange}) => {
+const Search = ({ onSearchChange }) => {
 
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
-        return fetch(`${GEO_API_URL}/cities?minPopulation = 10000000&namePrefix=${inputValue}`, geoApiOptions)
-        .then(response => response.json())
-        .then(response => console.log(response)) 
+        return fetch(`${GEO_API_URL}/cities?minPopulation = 1000000&namePrefix=${inputValue}`, geoApiOptions)
+        .then((response) => response.json())
+        .then((response) => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        lable: `${city.name}, ${city.country}`,
+                    };
+                }),
+            };
+        })
         .catch(err => console.error(err));
-    }
+    };
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
