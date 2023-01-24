5c0864d818482933a1366e3d93d690ee3747db3d
Dheeraj Soni <dheerajsoni7023@gmail.com>
2023-01-24 10:05:49 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '88d2acc9c9mshe7ffcff36e371d9p1d4408jsn379b777cd072',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
